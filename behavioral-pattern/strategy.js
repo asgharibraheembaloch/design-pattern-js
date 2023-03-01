c307576cf9f6a2696d5b3716a275bbb9ed4aebab
Asghar Ibraheem Baloch <asgharibraheembaloch@gmail.com>
2023-03-01 18:01:47 +0500

    created strategy pattern

---
+++ behavioral-pattern/strategy.js
@@ -0,0 +1,69 @@
+/*
+    Strategy Design Pattern -> https://www.youtube.com/watch?v=SicL4fYCz8w
+    Author: DevSage (Youtube) -> https://www.youtube.com/DevSage
+*/
+
+function Fedex(pkg)
+{
+  this.calculate = () =>
+  {
+    // Fedex calculations ...
+    return 2.45
+  }
+}
+
+function UPS(pkg)
+{
+  this.calculate = () =>
+  {
+    // UPS calculations ...
+    return 1.56
+  }
+}
+
+function USPS(pkg)
+{
+  this.calculate = () =>
+  {
+    // USPS calculations ...
+    return 4.5
+  }
+}
+
+// strategy encapsulate different strategy into one place
+// let's create object constructor which is going to serve as our context
+// because company method for calulating pacakage shipping has same name we can encapsulate that behaviour in shipping context here
+
+function Shipping()
+{
+  this.company = ''
+  this.setStrategy = company =>
+  {
+    this.company = company
+  }
+  this.calculate = pkg => {
+    return this.company.calculate(pkg)
+  }
+}
+
+const fedex = new Fedex()
+const ups = new UPS()
+const usps = new USPS()
+const shipping = new Shipping()
+const pkg = { from: "Alabama", to: "Georgia", weight: 1.56 } // Dummy package
+
+// before defining shipping function as strategy pattern we do like this
+// fedex.calculate(pkg)
+// ups.calculate(pkg)
+// usps.calculate(pkg)
+
+// strategy encapsulate different strategy into one place
+
+shipping.setStrategy(fedex)
+console.log("Fedex: " + shipping.calculate(pkg))
+
+shipping.setStrategy(ups)
+console.log("UPS: " + shipping.calculate(pkg))
+
+shipping.setStrategy(usps)
+console.log("USPS: " + shipping.calculate(pkg))
---
+++ creational-pattern/Singleton Pattern/after/index.js
@@ -0,0 +1,7 @@
+// singleton design pattern is pattern in which you work with only one instance of an object no matter 
+// how many new intstances you create they all points to same instance which you have create first
+import logFirstImplementation from './firstUse.js'
+import logSecondImplementation from './secondUse.js'
+
+logFirstImplementation()
+logSecondImplementation()
